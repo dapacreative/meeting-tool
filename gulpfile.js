ab287ace06e1090795809191c2d126c5f8c07bf7
Dan Smith <dan.smith@asynchrony.com>
2017-08-03 09:32:33 -0500

    Fix font issue, add gulfile copy, livereload tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,33 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    connect = require('gulp-connect'),
+    livereload = require('gulp-livereload');
 
 gulp.task('styles', function() {
-  gulp.src('src/scss/**/*.scss')
+  gulp.src('./src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(livereload());
+});
+
+//Connect Server
+gulp.task('connect', function() {
+  connect.server({
+    root: 'dist',
+    livereload: true
+  });
+});
+
+//Copy Files
+gulp.task('copy', function () {
+    gulp.src('./src/scss/base/font-files/**/*.{ttf,woff,eot,svg,woff2}')
+        .pipe(gulp.dest('./dist/css/font-files'));
 });
 
 //Watch task
-gulp.task('default',function() {
-  gulp.watch('src/scss/**/*.scss',['styles']);
+gulp.task('watch',function() {
+  livereload.listen();
+  gulp.watch('src/scss/**/*.scss',['styles', 'copy']);
 });
+
+gulp.task('default', ['connect', 'watch']);
